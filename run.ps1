e446d677ec5457916b89f3c9bbfc6285a8757af2
pyth laptop <py_official@mail.ru>
2025-02-15 17:39:39 +0300

    no com

--- run.ps1
+++ run.ps1
@@ -20,5 +20,6 @@ echo "##########################################################"
 # python3 app/scripts/main.py -launch_bot --name=Test --debug_mode=True --advanced_logging=True
 #python app/scripts/main.py -show_db -add_db --db_data="{'test':{'DB_NAME': 'pyth_database', 'DB_HOST':'89.46.131.209', 'DB_PORT':3306, 'DB_USER':'pyth', 'DB_PASS':'7vd6aOgn2808POd@QVoZmIXYzpSjrO~9@~RC@P4kIYgO5w6p0YJzuL}wNw8n}3ObEyWRnIrj'}}" -show_db
 #python app/scripts/main.py -launch_bot --name="Elder Mouse" --debug_mode=True --advanced_logging=True
-python app/scripts/main.py -test
+python app/scripts/main.py -launch_bot --name=Test --debug_mode=True --advanced_logging=True
+#python app/scripts/main.py -test
 pause
